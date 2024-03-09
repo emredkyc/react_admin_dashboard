6fd349d7b8ef3ef9867df14108fb6b8270606562
emredkyc <emredkyc@hotmail.com>
2024-03-09 09:57:59 +0100

    Add DashboardTotalCountCard component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,28 @@
-import { UpcomingEvents } from "@/components"
-import DealsChart from "@/components/home/deals-chart"
+import { UpcomingEvents, DealsChart, DashboardTotalCountCard } from "@/components"
+import { DASHBOARD_TOTAL_COUNTS_QUERY } from "@/graphql/queries"
+import { DashboardTotalCountsQuery } from "@/graphql/types"
+import { useCustom } from "@refinedev/core"
 import { Col, Row } from "antd"
 
 export const Home = () => {
+  const { data, isLoading } = useCustom<DashboardTotalCountsQuery>({
+    url: '',
+    method: 'get',
+    meta: {
+      gqlQuery: DASHBOARD_TOTAL_COUNTS_QUERY
+    }
+  }) 
   return (
     <div>
+      <Row gutter={[32, 32]}>
+        <Col xs={24} sm={24} xl={8}>
+          <DashboardTotalCountCard 
+            resource="companies"
+            isLoading={isLoading}
+            totalCount={data?.data.companies.totalCount}
+          />
+        </Col>
+      </Row>
       <Row
         gutter={[32, 32]}
         style={{
