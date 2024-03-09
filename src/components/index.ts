9829996410ecc8c870c61e6765667cec986e6db9
emredkyc <emredkyc@hotmail.com>
2024-03-09 09:56:26 +0100

    export DashboardTotalCountCard component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 import UpcomingEvents from "./home/upcoming-events";
 import DealsChart from "./home/deals-chart";
+import DashboardTotalCountCard from "./home/total-count-card";
 
-export { UpcomingEvents, DealsChart };
+export { UpcomingEvents, DealsChart, DashboardTotalCountCard};
