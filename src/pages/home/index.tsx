5db55d77c5f9bedaea54f238c3392b0ee1520f68
emredkyc <emredkyc@hotmail.com>
2024-03-09 09:00:41 +0100

    add UpcomingEvents component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,27 @@
+import { UpcomingEvents } from "@/components"
+import { Col, Row } from "antd"
+
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Row
+        gutter={[32, 32]}
+        style={{
+          marginTop: '32px'
+        }}
+      >
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: '460px'
+          }}
+        >
+          <UpcomingEvents />
+        </Col>
+      </Row>
+
+    </div>
   )
 }
-
-export default Home
