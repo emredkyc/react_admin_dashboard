25bb8922c56e068a359607a27701be4be20ce17e
emredkyc <emredkyc@hotmail.com>
2024-03-10 13:20:14 +0100

    exports task form components

--- src/components/index.ts
+++ src/components/index.ts
@@ -24,3 +24,10 @@ export {
 };
 export * from './tags/user-tag';
 export * from './text';
+export * from './accordion';
+export * from "./tasks/form/description";
+export * from "./tasks/form/due-date";
+export * from "./tasks/form/stage";
+export * from "./tasks/form/title";
+export * from "./tasks/form/users";
+export * from "./tasks/form/header";
